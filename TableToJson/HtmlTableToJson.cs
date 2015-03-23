9b7a3539faa06e157d7c7e91cff3de545ea1421e
Michael Yanni <michael_yanni@bradycorp.com>
2015-03-23 01:17:03 -0400

    Added basic implementation for flattening rowspan. An implementation for colspan is still needed.

--- TableToJson/HtmlTableToJson.cs
+++ TableToJson/HtmlTableToJson.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Script.Serialization;
 using CsQuery;
+using CsQuery.ExtensionMethods;
 
 namespace TableToJson
 {
@@ -63,17 +64,9 @@ namespace TableToJson
         private IEnumerable<string> GetHeadings(IDomObject table)
         {
             var firstRow = table.Cq().Find("tr:first").First();
-            if (Options.Headings.Any())
-            {
-                return Options.Headings;
-            }
-            else
-            {
-                return RowValues(firstRow, true);
-            }
-            //return Options.Headings.Any() ?
-            //    Options.Headings :
-            //    RowValues(firstRow, true);
+            return Options.Headings.Any() ?
+                Options.Headings :
+                RowValues(firstRow, true);
         }
 
         private bool IgnoredColumn(int index)
@@ -100,129 +93,123 @@ namespace TableToJson
             }
         }
 
-        private Dictionary<int, Dictionary<int, string>> ProcessRow(int rowIndex, IDomObject row, Dictionary<int, Dictionary<int, string>> temp)
+        private Dictionary<int, IDictionary<int, string>> ProcessRow(int rowIndex, CQ rowCq, Dictionary<int, IDictionary<int, string>> temp)
         {
-            if (rowIndex > 0 || Options.Headings.Any())
+            var cellIndex = 0;
+            if (!temp.ContainsKey(rowIndex))
             {
-                var rowCq = row.Cq();
-
-                var isEmpty = rowCq.Find("td").Length == rowCq.Find("td:empty").Length;
-
-                if ((rowCq.Is(":visible") || !Options.IgnoreHiddenRows) &&
-                    (!isEmpty || !Options.IgnoreEmptyRows) &&
-                    !Convert.ToBoolean((object)rowCq.Data("ignore")))
-                {
-                    var cellIndex = 0;
-                    if (!temp.ContainsKey(rowIndex))
-                    {
-                        temp[rowIndex] = new Dictionary<int, string>();
-                    }
-                    if (Options.IncludeRowId)
-                    {
-                        cellIndex = cellIndex + 1;
-                        temp[rowIndex][cellIndex] = rowCq.Attr("id") ?? String.Empty;
-                    }
-
-                    rowCq.Children().Each(cell =>
-                    {
-                        var cellCq = cell.Cq();
-
-                        // Process rowspans
-                        var length = 0;
-                        string text;
-                        if (cellCq.Filter("[rowspan]").Length > 0)
-                        {
-                            length = Convert.ToInt32(cellCq.Attr("rowspan"), 10) - 1;
-                            text = CellValues(cellIndex, cell);
-                            for (var i = 1; i <= length; i++)
-                            {
-                                if (temp[rowIndex + i] == null)
-                                {
-                                    temp[rowIndex + i] = new Dictionary<int, string>();
-                                }
-                                temp[rowIndex + i][cellIndex] = text;
-                            }
-                        }
-
-                        // Process colspans
-                        if (cellCq.Filter("[colspan]").Length > 0)
-                        {
-                            length = Convert.ToInt32(cellCq.Attr("colspan"), 10) - 1;
-                            text = CellValues(cellIndex, cell);
-                            for (var i = 1; i <= length; i++)
-                            {
-                                // Cell has both col and row spans.
-                                if (cellCq.Filter("[rowspan]").Length > 0)
-                                {
-                                    var length2 = Convert.ToInt32(cellCq.Attr("rowspan"), 10);
-                                    for (var j = 0; j < length2; j++)
-                                    {
-                                        temp[rowIndex + j][cellIndex + i] = text;
-                                    }
-                                }
-                                else
-                                {
-                                    temp[rowIndex][cellIndex + i] = text;
-                                }
-                            }
-                        }
-
-                        // Skip column if already defined
-                        //while (temp[rowIndex][cellIndex] != null)
-                        //while (cellIndex < temp[rowIndex].Count && temp[rowIndex][cellIndex] != null)
-                        while (temp[rowIndex].ContainsKey(cellIndex) && temp[rowIndex][cellIndex] != null)
-                        {
-                            cellIndex++;
-                        }
-
-                        //text = temp[rowIndex][cellIndex] ?? CellValues(cellIndex, cell);
-                        //text = ((cellIndex < temp[rowIndex].Count) ? temp[rowIndex][cellIndex] : null) ?? CellValues(cellIndex, cell);
-                        text = (temp[rowIndex].ContainsKey(cellIndex) ? temp[rowIndex][cellIndex] : null) ?? CellValues(cellIndex, cell);
-                        if (text != null)
-                        {
-                            temp[rowIndex][cellIndex] = text;
-                        }
-                        cellIndex++;
-                    });
-                }
+                temp[rowIndex] = new Dictionary<int, string>();
             }
+            if (Options.IncludeRowId)
+            {
+                cellIndex++;
+                temp[rowIndex][cellIndex] = rowCq.Attr("id") ?? String.Empty;
+            }
+
+            rowCq.Children().Each(cell =>
+            {
+                var cellCq = cell.Cq();
+                if (cellCq.Filter("[rowspan]").Length > 0)
+                {
+                    var length = Convert.ToInt32(cellCq.Attr("rowspan"), 10) - 1;
+                    cellCq.RemoveAttr("rowspan");
+
+                    var currentRow = rowCq;
+                    foreach (var i in Enumerable.Range(0, length))
+                    {
+                        currentRow = currentRow.Next("tr");
+                        currentRow.Children().Eq(cellIndex - 1).After(cell);
+                    }
+                }
+
+                // Process rowspans
+                //if (cellCq.Filter("[rowspan]").Length > 0)
+                //{
+                //    var length = Convert.ToInt32(cellCq.Attr("rowspan"), 10) - 1;
+                //    var text = CellValues(cellIndex, cell);
+                //    for (var i = 1; i <= length; i++)
+                //    {
+                //        if (!temp.ContainsKey(rowIndex + 1))
+                //        {
+                //            temp[rowIndex + i] = new Dictionary<int, string>();
+                //        }
+                //        temp[rowIndex + i][cellIndex] = text;
+                //    }
+                //}
+
+                //// Process colspans
+                //if (cellCq.Filter("[colspan]").Length > 0)
+                //{
+                //    var length = Convert.ToInt32(cellCq.Attr("colspan"), 10) - 1;
+                //    var text = CellValues(cellIndex, cell);
+                //    for (var i = 1; i <= length; i++)
+                //    {
+                //        // Cell has both col and row spans.
+                //        if (cellCq.Filter("[rowspan]").Length > 0)
+                //        {
+                //            var length2 = Convert.ToInt32(cellCq.Attr("rowspan"), 10);
+                //            for (var j = 0; j < length2; j++)
+                //            {
+                //                temp[rowIndex + j][cellIndex + i] = text;
+                //            }
+                //        }
+                //        else
+                //        {
+                //            temp[rowIndex][cellIndex + i] = text;
+                //        }
+                //    }
+                //}
+
+                // Skip column if already defined.
+                //while (temp[rowIndex].ContainsKey(cellIndex) && temp[rowIndex][cellIndex] != null)
+                //{
+                //    cellIndex++;
+                //}
+
+                var value = (temp[rowIndex].ContainsKey(cellIndex) ? temp[rowIndex][cellIndex] : null) ?? CellValues(cellIndex, cell);
+                if (value != null)
+                {
+                    temp[rowIndex][cellIndex] = value;
+                }
+                cellIndex++;
+            });
             return temp;
+        }
+
+        private bool IsValidRow(CQ rowCq, int rowIndex)
+        {
+            var isEmpty = rowCq.Find("td").Length == rowCq.Find("td:empty").Length;
+            return (rowIndex > 0 || Options.Headings.Any()) &&
+                ((rowCq.Is(":visible") || !Options.IgnoreHiddenRows) &&
+                (!isEmpty || !Options.IgnoreEmptyRows) &&
+                !Convert.ToBoolean(rowCq.Data("ignore")));
         }
 
         private IEnumerable<IDictionary<string, object>> Construct(IEnumerable<string> headings, IDomObject table)
         {
-            var temp = new Dictionary<int, Dictionary<int, string>>();
-            table.Cq().Children("tbody,*").Children("tr").Each((rowIndex, row) =>
+            var temp = new Dictionary<int, IDictionary<int, string>>();
+            table.Cq().Children("tbody,*").Children("tr").Elements
+            .Select(row => row.Cq())
+            .Where(IsValidRow)
+            .ForEach((rowCq, rowIndex) =>
             {
-                temp = ProcessRow(rowIndex, row, temp);
+                temp = ProcessRow(rowIndex, rowCq, temp);
             });
 
             var result = new List<IDictionary<string, object>>();
             foreach (var row in temp.OrderBy(r => r.Key).Select(r => r.Value.ToDictionary(x => x.Key, x => x.Value)))
             {
-                if (row != null)
-                {
-                    // Remove ignoredColumns / add onlyColumns
-                    var newRow = (Options.OnlyColumns.Any() || Options.IgnoreColumns.Any())
-                        ? row.Where((v, index) => !IgnoredColumn(index))
-                        : row;
+                // Remove ignoredColumns / add onlyColumns
+                var newRow = (Options.OnlyColumns.Any() || Options.IgnoreColumns.Any())
+                    ? row.Where((v, index) => !IgnoredColumn(index))
+                    : row;
 
-                    // Remove ignoredColumns / add onlyColumns if headings is not defined.
-                    var newHeadings = Options.Headings.Any() ? headings :
-                        headings.Where((v, index) => !IgnoredColumn(index));
+                // Remove ignoredColumns / add onlyColumns if headings is not defined.
+                var newHeadings = Options.Headings.Any() ? headings :
+                    headings.Where((v, index) => !IgnoredColumn(index));
 
-                    result.Add(ArraysToHash(newHeadings, newRow.Select(r => r.Value)).ToDictionary(x => x.Key, x => x.Value));
-                }
+                result.Add(ArraysToHash(newHeadings, newRow.OrderBy(r => r.Key).Select(r => r.Value)).ToDictionary(x => x.Key, x => x.Value));
             }
-
-            //var resultString = "";
-            //var serializer = new JavaScriptSerializer();
-            //resultString += serializer.Serialize(result);
-            ////foreach (var row in result)
-            ////{
-            ////    resultString += serializer.Serialize(row);
-            ////}
-            //return resultString;
             return result;
         }
 
