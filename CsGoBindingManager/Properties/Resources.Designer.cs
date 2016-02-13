85fb5febcf668ac7b6df65dfe12bfba28dce4a78
Michael Yanni <michael_yanni@bradycorp.com>
2016-02-13 05:39:53 -0800

    Updated assembly information for CsGoBindingManager.

--- CsGoBindingManager/Properties/Resources.Designer.cs
+++ CsGoBindingManager/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace LoginModule.Properties {
+namespace CsGoBindingManager.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace LoginModule.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LoginModule.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CsGoBindingManager.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- CsGoBindingManager/Properties/Settings.Designer.cs
+++ CsGoBindingManager/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace LoginModule.Properties {
+namespace CsGoBindingManager.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
